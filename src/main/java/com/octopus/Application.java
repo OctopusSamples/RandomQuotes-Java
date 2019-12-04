8aa713d9dd9bded6192e466a204820051bdf03ab
Matthew Casperson <matthewcasperson@gmail.com>
2019-12-04 14:13:44 +1000

    Fixed a spring issue deploying a war file

--- src/main/java/com/octopus/Application.java
+++ src/main/java/com/octopus/Application.java
@@ -2,12 +2,19 @@ package com.octopus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 
 }
